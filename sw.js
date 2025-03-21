caace0514fe10f8b17d9ebc517585960c9fcb947
Alex <Alex.007.Writer@gmail.com>
2025-03-21 17:09:18 +0200

    2

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/img/background.jpg',
+                '/img/avatar.webp',
+                // Додайте інші ресурси
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
